37d577a6dbad39ae99c14a3f6a02efef423454f8
Amith <Amith@Amiths-MacBook-Pro.local>
2017-07-14 12:26:10 +0530

    With Rest Service

---
+++ JflexExample/src/main/java/com/prysm/controller/SearchController.java
@@ -0,0 +1,23 @@
+package com.prysm.controller;
+
+import java.io.IOException;
+
+import org.jflex.example.JFlexExample;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.prysm.models.Result;
+
+@RestController
+public class SearchController {
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public Result getResult(@RequestParam(value = "query") String query) throws IOException {
+		Result result = new Result();
+		result.setQuery(query);
+		JFlexExample.getUrl(query.toLowerCase());
+		result.setResponse("your query: " + query);
+		return result;
+	}
+}
---
+++ JflexExample/src/main/java/com/prysm/models/Result.java
@@ -0,0 +1,53 @@
+package com.prysm.models;
+
+public class Result {
+	private String query;
+	private Object response;
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((query == null) ? 0 : query.hashCode());
+		result = prime * result + ((response == null) ? 0 : response.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Result other = (Result) obj;
+		if (query == null) {
+			if (other.query != null)
+				return false;
+		} else if (!query.equals(other.query))
+			return false;
+		if (response == null) {
+			if (other.response != null)
+				return false;
+		} else if (!response.equals(other.response))
+			return false;
+		return true;
+	}
+
+	public String getQuery() {
+		return query;
+	}
+
+	public void setQuery(String query) {
+		this.query = query;
+	}
+
+	public Object getResponse() {
+		return response;
+	}
+
+	public void setResponse(Object response) {
+		this.response = response;
+	}
+}
